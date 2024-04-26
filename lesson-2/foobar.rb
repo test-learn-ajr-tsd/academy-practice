7b227b7197c76bb4e1a91a2013f25a22421df728
test-learn-ajr-tsd <dim.sergeev2000@vk.com>
2024-04-26 17:55:57 +0300

    Lesson 2

---
+++ lesson-2/foobar.rb
@@ -0,0 +1,5 @@
+def foobar(x, y)
+	return y if x == 20 || y == 20
+	
+	return x + y	
+end
---
+++ lesson-2/greetings.rb
@@ -0,0 +1,14 @@
+def greetings
+	puts 'Введите имя: '
+	first_name = gets.chomp
+	puts 'Введите фамилию: '
+	last_name = gets.chomp
+	puts 'Введите возраст: '
+	age = gets.chomp.to_i
+
+	greeting_str = 'Привет, ' + first_name + ' ' + last_name + '. '
+	return greeting_str + 'Тебе меньше 18 лет, но начать учиться программировать никогда не рано!' if age < 18
+	return greeting_str + 'Самое время заняться делом!' if age >= 18
+end
+
+#puts greetings
