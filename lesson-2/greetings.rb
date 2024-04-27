ed86ed5ca6748a2468daa671cb403d36a4eee71b
test-learn-ajr-tsd <dim.sergeev2000@vk.com>
2024-04-27 22:22:20 +0300

    Lesson 2

--- lesson-2/greetings.rb
+++ lesson-2/greetings.rb
@@ -3,8 +3,10 @@ def greetings
 	first_name = gets.chomp
 	puts 'Введите фамилию: '
 	last_name = gets.chomp
-	puts 'Введите возраст: '
-	age = gets.chomp.to_i
+	puts 'Введите возраст: ' 
+	begin
+		age = gets.chomp.to_i 	
+	end	until age > 0	
 
 	greeting_str = 'Привет, ' + first_name + ' ' + last_name + '. '
 	return greeting_str + 'Тебе меньше 18 лет, но начать учиться программировать никогда не рано!' if age < 18
