9ab55ec4d553e3f8a4b8b26f8326f1b606e72363
test-learn-ajr-tsd <dim.sergeev2000@vk.com>
2024-05-15 14:38:43 +0300

    Lesson 5

---
+++ lesson-5/CashMachine_spec.rb
@@ -0,0 +1,36 @@
+require 'rspec'
+require_relative 'CashMachine.rb'
+
+RSpec.describe 'Тест работы со классом CashMachine' do	
+	it 'Тест - инициализация' do 
+		cash_machine = CashMachine.new
+		expect(cash_machine.баланс).to eq 100.0
+	end
+
+	it 'Тест - инициализация с наличием файла баланса' do 
+		cash_machine = CashMachine.new
+		expect(cash_machine.баланс).to eq File.read('balance.txt').to_f #chomp.
+	end
+
+	cash_machine = CashMachine.new
+	it 'Тест - увеличение баланса' do		
+		cash_machine.увеличитьБаланс(20.0)
+		expect(cash_machine.баланс).to eq 120.0
+	end
+
+	it 'Тест - уменьшение баланса' do 		
+		cash_machine.уменьшитьБаланс(40.0)
+		expect(cash_machine.баланс).to eq 80.0
+	end
+	
+	it 'Тест - запись баланса' do			
+		cash_machine.записатьБаланс
+		expect(File.read('balance.txt').to_f).to eq 80.0
+	end
+
+	it 'Тест - попытка списать больше имеющегося баланса' do		
+		cash_machine.уменьшитьБаланс(200.0)
+		expect(cash_machine.баланс).to eq 80.0
+	end
+end
+
