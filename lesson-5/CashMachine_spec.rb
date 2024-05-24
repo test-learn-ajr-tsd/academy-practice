bd58b002c45cc8767b23212fcf6323ce9b5ce9b7
test-learn-ajr-tsd <dim.sergeev2000@vk.com>
2024-05-24 18:30:38 +0300

    Lesson 5 repaired

--- lesson-5/CashMachine_spec.rb
+++ lesson-5/CashMachine_spec.rb
@@ -2,6 +2,7 @@ require 'rspec'
 require_relative 'CashMachine.rb'
 
 RSpec.describe 'Тест работы со классом CashMachine' do	
+	before { File.delete('balance.txt') if File.exist?('balance.txt') }
 	it 'Тест - инициализация' do 
 		cash_machine = CashMachine.new
 		expect(cash_machine.баланс).to eq 100.0
@@ -30,6 +31,26 @@ RSpec.describe 'Тест работы со классом CashMachine' do
 
 	it 'Тест - попытка списать больше имеющегося баланса' do		
 		cash_machine.уменьшитьБаланс(200.0)
+		expect(cash_machine.баланс).to eq 80.0
+	end
+
+	it 'Тест - попытка списать "отрицательный" баланс' do		
+		cash_machine.уменьшитьБаланс(-20.0)
+		expect(cash_machine.баланс).to eq 80.0
+	end
+
+	it 'Тест - попытка списать "странный" баланс' do		
+		cash_machine.уменьшитьБаланс('sffdsxcvxcv')
+		expect(cash_machine.баланс).to eq 80.0
+	end
+
+	it 'Тест - попытка увеличить "отрицательный" баланс' do		
+		cash_machine.увеличитьБаланс(-20.0)
+		expect(cash_machine.баланс).to eq 80.0
+	end
+
+	it 'Тест - попытка увеличить "странный" баланс' do		
+		cash_machine.увеличитьБаланс('sffdsxcvxcv')
 		expect(cash_machine.баланс).to eq 80.0
 	end
 end
