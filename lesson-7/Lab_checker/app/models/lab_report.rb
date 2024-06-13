84cb2306d0d6dab27faf41381ed2220e303e1ae2
test-learn-ajr-tsd <dim.sergeev2000@vk.com>
2024-06-13 15:44:32 +0300

    Test coverage

--- lesson-7/Lab_checker/app/models/lab_report.rb
+++ lesson-7/Lab_checker/app/models/lab_report.rb
@@ -1,7 +1,7 @@
 class LabReport < ApplicationRecord
 	belongs_to :user
 
-	validates :title, presence: true, length: { maximum: 120, minimum: 3 }
+	validates :title, presence: true, length: { maximum: 120 } #minimum: 3
 	validates :description, presence: true
 	validates :grade, presence: true, length: { maximum: 10 }
 end
