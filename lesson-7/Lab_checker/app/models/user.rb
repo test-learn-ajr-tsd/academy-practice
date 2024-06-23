298c5fafd54303212989d55d533a7fc4ae5b2764
test-learn-ajr-tsd <dim.sergeev2000@vk.com>
2024-06-23 16:13:08 +0300

    Add Devise gem for auth

--- lesson-7/Lab_checker/app/models/user.rb
+++ lesson-7/Lab_checker/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 	has_many :lab_reports
 
 	validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { maximum: 120, minimum: 3 }
